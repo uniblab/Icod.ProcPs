07c892c0ed1e4cb3423f4c763df49560f1b1f3d3
uniblab <uniblab@hotmail.com>
2026-08-26 17:41:44 -0400

    preamble

--- procps/Program.cs
+++ procps/Program.cs
@@ -1,3 +1,24 @@
+/*
+	procps
+	Route Icod.ProcPs utility commands from a single executable.
+	Copyright (C) 2026  Timothy J. Bruce <uniblab@hotmail.com>
+*/
+
+/*
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 namespace Icod.ProcPs.Router;
 
 /// <summary>Provides the process entry point for <c>procps</c>.</summary>
